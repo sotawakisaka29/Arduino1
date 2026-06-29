7ab3c93aef4bfb0cb42fd0623d762958ea3e1ea7
{sotawakisaka29} <{sota.wakisaka29@gmail.com}>
2026-06-29 13:19:53 +0900

    update

--- ProgramA.ino/ProgramA.ino.ino
+++ ProgramA.ino/ProgramA.ino.ino
@@ -9,6 +9,7 @@ void setup() {
   Serial.begin(9600);
 
   buzzer.playOn(); 
+  Serial.println("Input Available");
   getCommand();
 
   Serial.println("Push button to start.");
@@ -21,30 +22,39 @@ void loop() {
 
 }
 
+
 void getCommand(void){
-  if (Serial.available() > 0) {
-    char input = Serial.read();
+  cmdIndex = 0;
 
-    if (cmdIndex >= MAX_COMMAND) {
-      cmdIndex = 0;
-      Serial.println("Over Max Command. Delete All Command!");
-    }
+  while (1) {
+    if (Serial.available() > 0) {
+      char input = Serial.read();
 
-    if (input == 'd') {
-      cmdIndex = 0;
-      Serial.println("Delete All Command!");
-    }
-    else if (input == 'r' || input == 'l' || input == 'f') {
-      command[cmdIndex] = input;
-      cmdIndex++;
-    }
-    else if (input == '.') {
-      command[cmdIndex] = '\0'; 
-      Serial.println(command);
-    }
-    else {
-      Serial.println("Wrong Command!");
+      if (input == '\n' || input == '\r') {
+              continue;
+      }
+
+      if (cmdIndex >= MAX_COMMAND) {
+        cmdIndex = 0;
+        Serial.println("Over Max Command. Delete All Command!");
+      }
+
+      if (input == 'd') {
+        cmdIndex = 0;
+        Serial.println("Delete All Command!");
+      }
+      else if (input == 'r' || input == 'l' || input == 'f') {
+        command[cmdIndex++] = input;
+      }
+      else if (input == '.') {
+        command[cmdIndex] = '\0';
+        Serial.print("Command: ");
+        Serial.println(command);
+        break;  // ← 入力完了で抜ける
+      }
+      else {
+        Serial.println("Wrong Command!");
+      }
     }
   }
-}
 }
